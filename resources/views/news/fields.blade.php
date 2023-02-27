f8b2518b4abbcf295341d541908951a3c76c7baa
Raxmatilla <56291918+Raxmatilla97@users.noreply.github.com>
2023-02-27 11:55:19 +0500

    kichik o'zgarish

--- resources/views/news/fields.blade.php
+++ resources/views/news/fields.blade.php
@@ -1,13 +1,13 @@
 <!-- Title Field -->
 <div class="row">
     <div class="form-group col-sm-6">
-        {!! Form::label('title', 'Title:') !!}
+        {!! Form::label('title', 'Yangilikning nomlanishini qisqa qilib yozing:') !!}
         {!! Form::text('title', null, ['class' => 'form-control']) !!}
     </div>
     
     <!-- Img Field -->
     <div class="form-group col-sm-6">
-        {!! Form::label('img', 'Img:') !!}
+        {!! Form::label('img', 'Yangiliks uratini yuklang:') !!}
         {!! Form::file('img',['id' => 'image', 'multiple', 'data-allow-reorder' => 'true', 'data-max-file-size' => '3MB', 'data-max-files' => '1']) !!}
     </div>
     <div class="clearfix"></div>
@@ -17,7 +17,7 @@
 
 <!-- Content Field -->
 <div class="form-group col-sm-12 col-lg-12">
-    {!! Form::label('content', 'Content:') !!}
+    {!! Form::label('content', 'Asosiy qism:') !!}
     {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
 </div>
 
